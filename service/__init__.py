b5630ece69e4b5e4835d3ccce0e007347aac7bc8
Harish <harishharidas93@gmail.com>
2025-11-19 12:05:30 -0500

    added secuity headers

--- service/__init__.py
+++ service/__init__.py
@@ -8,6 +8,7 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
@@ -22,6 +23,8 @@ from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
+
+talisman = Talisman(app)
 
 app.logger.info(70 * "*")
 app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
