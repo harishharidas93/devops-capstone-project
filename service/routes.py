962eff1414730aa6e869df454fe32cce2cc8eec0
Harish Haridas <65101283+harishharidas93@users.noreply.github.com>
2025-11-19 15:58:54 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,10 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 # ... place you code here to LIST accounts ...
+
+
 
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
@@ -79,6 +82,7 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
 # ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -95,6 +99,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 # ... place you code here to UPDATE an account ...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
@@ -114,6 +119,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
